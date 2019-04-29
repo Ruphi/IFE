68b843531ce67cd58e5c6acfc3b34e1a1c543fd0
ruphi <morrisfan@126.com>
2019-04-29 08:55:54 +0800

    add color-picker

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,10 +1,32 @@
-import _ from 'lodash';
 import './index.scss';
 
 const div = document.createElement('div');
 
-const compiled = _.template('hello <%= user %>!');
-div.innerHTML = compiled({ 'user': 'fred' });
-div.classList.add('blue');
+const xhr = new XMLHttpRequest();
+xhr.open('GET', '../static/data/project-list.json', true);
+xhr.onreadystatechange = function (){
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        const res = JSON.parse(xhr.response);
 
-document.body.appendChild(div);
+        const h2 = document.createElement('h2');
+        h2.innerHTML = res.title;
+
+        const liFragment = document.createDocumentFragment();
+        for (let i = 0; i < res.list.length; i++) {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+            a.innerHTML = res.list[i]['name'];
+            a.href = res.list[i]['url'];
+
+            li.appendChild(a);
+            liFragment.appendChild(li);
+        }
+
+        div.appendChild(h2);
+        div.appendChild(liFragment);
+        document.body.appendChild(div);
+    }else {
+
+    }
+}
+xhr.send();
